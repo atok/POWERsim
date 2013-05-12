1cd2b94f6efbe4aa5e139a279db3d24a117fd07a
Khajiit333 <marcin.marzynski@gmail.com>
2013-05-12 17:57:02 +0200

    Human-specific properties added to Human class

--- src/agh/powerSim/simulation/actors/humans/Human.java
+++ src/agh/powerSim/simulation/actors/humans/Human.java
@@ -12,6 +12,14 @@ public class Human extends BaseHuman {
     public static enum State {Sleeping, Awake};
     private State state = State.Awake;
 
+    //characteristics
+    int darknessTreshold = 300;
+    int entertainment;
+    int hunger;
+    int workDuty;
+    int cleaner;
+    int coldTreshold;
+
     ClockActor.TimeSignal currentTime;
 
     public Human(ActorRef house, ArrayList<DeviceToken> devices) {
@@ -50,13 +58,61 @@ public class Human extends BaseHuman {
     private void lightActions(double lightLevel) {
         log.warning("lightLevel " + lightLevel);
         if(state == State.Awake) {
-            if(lightLevel < 300) {
+            if(lightLevel < darknessTreshold) {
                 log.warning("More light!!!");
                 requestMoreLight();
             }
         } else if(state == State.Sleeping) {
 
         }
+    }
+
+    public int getDarknessTreshold() {
+        return darknessTreshold;
+    }
+
+    public void setDarknessTreshold(int darknessTreshold) {
+        this.darknessTreshold = darknessTreshold;
+    }
+
+    public int getEntertainment() {
+        return entertainment;
+    }
+
+    public void setEntertainment(int entertainment) {
+        this.entertainment = entertainment;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public int getWorkDuty() {
+        return workDuty;
+    }
+
+    public void setWorkDuty(int workDuty) {
+        this.workDuty = workDuty;
+    }
+
+    public int getCleaner() {
+        return cleaner;
+    }
+
+    public void setCleaner(int cleaner) {
+        this.cleaner = cleaner;
+    }
+
+    public int getColdTreshold() {
+        return coldTreshold;
+    }
+
+    public void setColdTreshold(int coldTreshold) {
+        this.coldTreshold = coldTreshold;
     }
 
     public static class SomeCustomEvent {}
